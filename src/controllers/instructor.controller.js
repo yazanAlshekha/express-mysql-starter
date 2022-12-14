2a9248eb69f4a6d44a07d4456e436126fe27e5fe
yazanAlshekha <yazan.alshekha@nadsoft.net>
2022-12-14 14:29:21 +0000

    route : instructorRate is done

--- src/controllers/instructor.controller.js
+++ src/controllers/instructor.controller.js
@@ -1,51 +1,54 @@
 const catchAsync = require('../utils/catchAsync');
 const pick = require('../utils/pick');
 const httpStatus = require('http-status');
-const {instructorService} = require('../services');
+const { instructorService } = require('../services');
 
+exports.getCourses = catchAsync(async (req, res) => {
+  const payload = pick(req.query, ['instructorId']);
+  const result = await instructorService.getCourses(payload);
+  res.status(httpStatus.OK).send(result);
+});
 
-exports.getCourses = catchAsync(async(req,res)=>{
-    const payload = pick (req.query , ['instructorId'] );
-    const result =await instructorService.getCourses(payload);
-    res.status(httpStatus.OK).send(result)
-})
+exports.createCourse = catchAsync(async (req, res) => {
+  const payload = pick(req.body, ['instructorId', 'courseName']);
+  const result = await instructorService.createCourse(payload);
+  res.status(httpStatus.CREATED).send('course has been created');
+});
 
-exports.createCourse = catchAsync(async(req,res)=>{
-    const payload = pick (req.body , ['instructorId','courseName'] );
-    const result =await instructorService.createCourse(payload);
-    res.status(httpStatus.CREATED).send("course has been created")
-})
+exports.deleteCourse = catchAsync(async (req, res) => {
+  const payload = pick(req.body, ['instructorId', 'courseId']);
+  const result = await instructorService.deleteCourse(payload);
+  console.log('🚀 ~ file: instructor.controller.js:17 ~ exports.deleteCourse ~ result', result);
 
-exports.deleteCourse = catchAsync(async(req,res)=>{
-    const payload = pick (req.body , ['instructorId','courseId'] );
-    const result =await instructorService.deleteCourse(payload);
-    console.log("🚀 ~ file: instructor.controller.js:17 ~ exports.deleteCourse ~ result", result)
-    
-    res.status(httpStatus.OK).send("course has been deleted")
-})
+  res.status(httpStatus.OK).send('course has been deleted');
+});
 
-exports.editCourse = catchAsync(async(req,res)=>{
-    const payload = pick (req.body , ['instructorId','courseId','courseName'] );
-    const result =await instructorService.editCourse(payload);
-    res.status(httpStatus.CREATED).send("course has been updated")
-})
+exports.editCourse = catchAsync(async (req, res) => {
+  const payload = pick(req.body, ['instructorId', 'courseId', 'courseName']);
+  const result = await instructorService.editCourse(payload);
+  res.status(httpStatus.CREATED).send('course has been updated');
+});
 
-exports.courseLikes = catchAsync(async(req,res)=>{
-    const payload = pick(req.query , ['courseId'] );
-    const result = await instructorService.courseLikes(payload);
-    res.status(httpStatus.OK).send(result);
-})
+exports.courseLikes = catchAsync(async (req, res) => {
+  const payload = pick(req.query, ['courseId']);
+  const result = await instructorService.courseLikes(payload);
+  res.status(httpStatus.OK).send(result);
+});
 
+exports.courseReview = catchAsync(async (req, res) => {
+  const payload = pick(req.query, ['courseId']);
+  const result = await instructorService.courseReview(payload);
+  res.status(httpStatus.OK).send(result);
+});
 
-exports.courseReview = catchAsync(async(req,res)=>{
-    const payload = pick(req.query , ['courseId'] );
-    const result = await instructorService.courseReview(payload);
-    res.status(httpStatus.OK).send(result);
-})
+exports.courseComments = catchAsync(async (req, res) => {
+  const payload = pick(req.query, ['courseId']);
+  const result = await instructorService.courseComments(payload);
+  res.status(httpStatus.OK).send(result);
+});
 
-exports.courseComments = catchAsync(async(req,res)=>{
-    const payload = pick(req.query , ['courseId'] );
-    const result = await instructorService.courseComments(payload);
-    res.status(httpStatus.OK).send(result);
-})
-
+exports.instructorRate = catchAsync(async (req, res) => {
+  const payload = pick(req.query, ['instructorId']);
+  const result = await instructorService.instructorRate(payload);
+  res.status(httpStatus.OK).send(result);
+});
--- src/routes/v1/instructor.route.js
+++ src/routes/v1/instructor.route.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const validate = require('../../middlewares/validate');
-const {instructorController} = require('../../controllers');
-const {instructorValidation} = require('../../validations')
-
-
+const { instructorController } = require('../../controllers');
+const { instructorValidation } = require('../../validations');
 
 const router = express.Router();
 
-router.route('/crudCourse')
-.get(validate(instructorValidation.getCourses), instructorController.getCourses)
-.post(validate(instructorValidation.createCourse) ,instructorController.createCourse )
-.put(validate(instructorValidation.editCourse),instructorController.editCourse)
-.delete(validate(instructorValidation.deleteCourse) ,instructorController.deleteCourse )
+router
+  .route('/crudCourse')
+  .get(validate(instructorValidation.getCourses), instructorController.getCourses)
+  .post(validate(instructorValidation.createCourse), instructorController.createCourse)
+  .put(validate(instructorValidation.editCourse), instructorController.editCourse)
+  .delete(validate(instructorValidation.deleteCourse), instructorController.deleteCourse);
 
-router.route('/courseLikes')
-.get(validate(instructorValidation.courseLikes), instructorController.courseLikes)
+router.route('/courseLikes').get(validate(instructorValidation.courseLikes), instructorController.courseLikes);
 
-router.route('/courseReviews')
-.get(validate(instructorValidation.courseReview), instructorController.courseReview)
+router.route('/courseReviews').get(validate(instructorValidation.courseReview), instructorController.courseReview);
 
-router.route('/courseComments')
-.get(validate(instructorValidation.courseComments), instructorController.courseComments)
+router.route('/courseComments').get(validate(instructorValidation.courseComments), instructorController.courseComments);
 
-
-module.exports=router
+router.route('/instructorRate').get(validate(instructorValidation.instructorRate), instructorController.instructorRate);
+module.exports = router;
